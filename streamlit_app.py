9868c1db4a433f9f93675abb64cd6f36344bf9db
Sandeep jindal <jindalsandeep47@gmail.com>
2023-02-27 19:56:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,5 +39,11 @@ chart_data = pd.DataFrame(
     columns=["a", "b", "c"])
 
 st.bar_chart(chart_data)
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
     
 
